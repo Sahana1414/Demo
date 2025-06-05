a4fe59009a21ffaa7140e23fe1375334bb72cba6
CAS EXAM <casexam@kgisledu.com>
2025-06-05 13:00:37 +0530

    day1 completed

---
+++ Day1task/task1.java
@@ -0,0 +1,17 @@
+package addon_dsa;
+
+import java.util.Arrays;
+
+public class task1 {
+	public static void main(String args[]) {
+		int []arr= {1,2,3,4,5};
+		System.out.println(Arrays.toString(arr));
+		
+		for(int i=0;i<5;i++)
+		{
+			System.out.println(arr[i]);
+			
+		}
+	}
+
+}
